15d4607135d9642e50e5a3e1994fe32ea8a04371
gufra123cj <gufrancj@gmail.com>
2025-11-11 18:50:16 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using namespace game;
 static GameServer* g_server = nullptr;
 //https://www.sfml-dev.org/tutorials/3.0/getting-started/visual-studio/#creating-and-configuring-an-sfml-project
 //https://www.raylib.com
-void signalHandler(int signal) {
+static void signalHandler(int signal) {
     if (g_server) {
         std::cout << "\nShutting down server..." << std::endl;
         g_server->shutdown();
